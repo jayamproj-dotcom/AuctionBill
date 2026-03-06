01ea9d11aa44ae12d0af087e3769da30b6d015e9
jayamproj-dotcom <jayamproj@gmail.com>
2026-03-06 12:33:16 +0530

    setwes

--- src/api/auctionApi.js
+++ src/api/auctionApi.js
@@ -51,47 +51,6 @@ export const toggleProductStatus = async (id) => {
     }
 };
 
-// BUYERS
-export const getBuyers = async (vendorId) => {
-    try {
-        const response = await api.get(`/auction/buyers/list/${vendorId}`);
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
-export const addBuyer = async (data) => {
-    try {
-        const response = await api.post('/auction/buyers/add', data);
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
-export const updateBuyer = async (id, data) => {
-    try {
-        const response = await api.put(`/auction/buyers/update/${id}`, data);
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
-export const deleteBuyer = async (id) => {
-    try {
-        const response = await api.delete(`/auction/buyers/delete/${id}`);
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
 // TRANSACTIONS
 export const recordSale = async (data) => {
     try {
@@ -106,27 +65,6 @@ export const recordSale = async (data) => {
 export const getTransactions = async (vendorId, date) => {
     try {
         const response = await api.get(`/auction/transactions/list/${vendorId}`, { params: { date } });
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
-// BUYER PAYMENTS
-export const getBuyerPayments = async (vendorId, buyerId) => {
-    try {
-        const response = await api.get(`/auction/payments/list/${vendorId}`, { params: { buyerId } });
-        return response.data;
-    } catch (error) {
-        if (error.response && error.response.data) throw error.response.data;
-        throw error;
-    }
-};
-
-export const addBuyerPayment = async (data) => {
-    try {
-        const response = await api.post('/auction/payments/add', data);
         return response.data;
     } catch (error) {
         if (error.response && error.response.data) throw error.response.data;
---
+++ src/api/buyerApi.js
@@ -0,0 +1,63 @@
+import api from './api';
+
+// BUYERS
+export const getBuyers = async (vendorId) => {
+    try {
+        const response = await api.get(`/buyer/list/${vendorId}`);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
+
+export const addBuyer = async (data) => {
+    try {
+        const response = await api.post('/buyer/add', data);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
+
+export const updateBuyer = async (id, data) => {
+    try {
+        const response = await api.put(`/buyer/update/${id}`, data);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
+
+export const deleteBuyer = async (id) => {
+    try {
+        const response = await api.delete(`/buyer/delete/${id}`);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
+
+// BUYER PAYMENTS
+export const getBuyerPayments = async (vendorId, buyerId) => {
+    try {
+        const response = await api.get(`/buyer/payments/list/${vendorId}`, { params: { buyerId } });
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
+
+export const addBuyerPayment = async (data) => {
+    try {
+        const response = await api.post('/buyer/payments/add', data);
+        return response.data;
+    } catch (error) {
+        if (error.response && error.response.data) throw error.response.data;
+        throw error;
+    }
+};
