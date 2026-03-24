a1a031d8f3965cdd13f98d1749e4f329c3ee4af4
jayamwebbo6-ops <jayamwebbo6@gmail.com>
2026-03-24 14:41:10 +0530

    hfugfugu

--- src/components/Billing/Billing.jsx
+++ src/components/Billing/Billing.jsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useSelector } from "react-redux";
 import {
-  FileText,
   Download,
   User,
   ShoppingCart,
   History,
   Handshake,
   Calendar,
-  ChevronRight,
-  Printer,
   Loader,
 } from "lucide-react";
 import { getSellers } from "../../api/sellerApi";
